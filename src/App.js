b90153686674c9d5ee8cee4b0e51b15693da8627
Bickey Jaisawal <jvickram2@gmail.com>
2026-01-14 03:46:13 +0530

    Finished integrating gemini-3-flash-preview api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import { useState } from 'react';
 import './App.css';
-import {Chat} from './Components/Chats/Chats'
-import {Controls} from './Components/Controls/Controls'
+import {Chat} from './components/Chats/Chats'
+import {Controls} from './components/Controls/Controls'
+import { GoogleGenAI } from "@google/genai";
+
+const api_key = process.env.REACT_APP_API_KEY
+const googleai = new GoogleGenAI({ apiKey:api_key});
 
 function App() {
-  const [message, setMessage] = useState([])
+  const [message, setMessages] = useState([])
 
-  function handleContentSend(content){
-    setMessage((prevMessages)=>[...prevMessages, {content,role:'user'}])
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
+    try {
+      
+      const result = await googleai.models.generateContent({
+        model: "gemini-3-flash-preview",
+        contents: content,
+      });
+
+      addMessage({ content: result.text(), role: "assistant" });
+    } catch (error) {
+      addMessage({
+        content: "Sorry, I couldn't process your request due to quota reached or technical issues. Please try again!",
+        role: "system",
+      });
+    }
+
   }
 
   return (
